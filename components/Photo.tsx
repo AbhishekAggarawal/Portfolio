aa1fa419d2b3dbdb0805a448b9c4eeb95e97999b
Abhishek Aggarwal <abhishekmonu2000@gmail.com>
2026-05-16 09:52:50 +0530

    Update Photo.tsx

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -22,7 +22,7 @@ const Photo = () => {
                  className="  w-[256px] h-[300px] xl:w-[504px] xl:h-[502px]  absolute ">
                 <Image 
 
-                src="/assets/picture.png" 
+                src="/assets/imagge.png" 
                 priority 
                 quality={100} 
                 fill
